c69b9c61b6034f345d87a3efa6b708bd8f44b979
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2024-08-13 00:40:27 +0400

    new commit

---
+++ ConditionalStatments.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ConditionalStatments {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter You're Age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();        
+
+        if(age > 18){
+            System.out.println("You Older Than 18  ");
+        }else if(age == 18){
+            System.out.println("You Are 18 Years old");
+        }
+        else if(age < 0){
+            System.out.println("Bro You Are not born");
+        }
+        else{
+            System.out.println("You Are Younger than 18 ");
+        }
+        
+
+        
+        scanner.close();
+    }
+}
---
+++ random.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class random {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        Random random = new Random();
+
+        System.out.println("Enter number that you want to be maximum: ");
+        int userinput = scanner.nextInt();
+        scanner.nextLine();
+        
+        
+
+        int x = random.nextInt(userinput)+1;
+
+        System.out.println(x);
+        scanner.close();
+    }
+    
+}
